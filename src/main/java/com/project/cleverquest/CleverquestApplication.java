bfca8cc4a910ffe51feeea2a030c67d2267b6a7b
Luizhnrs <Luizhnrsg@gmail.com>
2025-10-18 15:12:08 -0300

    FEAT: Docker compose created

---
+++ src/main/java/com/project/cleverquest/CleverquestApplication.java
@@ -0,0 +1,13 @@
+package com.project.cleverquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cleverquest/CleverquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cleverquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
